ac3c79d0cdb587281a7c01daf5080c2595b1c3b4
ccie57960 <pedroangeld@gmail.com>
2020-08-06 09:13:02 -0400

    some changes toconstants.py and updater.py

--- scripts/constants.py
+++ scripts/constants.py
@@ -5,32 +5,27 @@ from sys import platform
 class Constants():
     '''Constant variables like path for the project'''
     def __init__(self):
+        self.os_delimiter = "\\"
         if "win" in platform:
             self.path_root = r"C:\Users\Pedro\project_b\\"
         else:
-            self.path_root = r"/home/peter/Desktop/rm_monthly/testgit/"
+            self.os_delimiter = "/"
+            self.path_root = r"/media/Data/MyDoc/Python/win_registry/"
 
     def files(self):
         '''return dictionary including the path of all files in "files path"'''
-        files = {}
         f = ("local.json", "meta.json", "mi_proxy.xml", "source.zip")
-
-        for i in f:
-            files.update({i:f"{self.path_root}{i}"})
-        return files
+        return {i:f"{self.path_root}files{self.os_delimiter}{i}" for i in f}
 
     def scripts(self):
         '''return dictionary including the path of all files in "scripts path"'''
-        files = {}
         f = ("constants.py", "launcher.vbs", "updater.py", "win_registry.py")
-
-        for i in f:
-            files.update({i:f"{self.path_root}{i}"})
-        return files
+        return {i:f"{self.path_root}scripts{self.os_delimiter}{i}" for i in f}
 
     def url(self):
         '''return dictionary including the URLs"'''
         return {"git_source": r"https://github.com/ccie57960/win_registry/archive/source.zip",
+        "git_master": r"https://github.com/ccie57960/win_registry/archive/master.zip",
         "drive_base": (r"https://drive.google.com/uc?id=", r"&export=download")
 
         }
--- scripts/updater.py
+++ scripts/updater.py
@@ -4,52 +4,55 @@ import urllib.request
 import json
 from zipfile import ZipFile
 from constants import Constants
+from os import remove
 
 class Updater():
     def __init__(self):
-        # self._urlbase = (r"https://drive.google.com/uc?id=", r"&export=download")
-        # self._url = r"https://github.com/ccie57960/win_registry/archive/source.zip"
-        # self._path = r"/home/peter/Desktop/rm_monthly/testgit/source.zip"
-        # self._path_root = r"C:\Users\Pedro\project_b"
-        # self._path_root = r"/home/peter/Desktop/rm_monthly/testgit/"
         c = Constants()
         self.files = c.files()
         self.url = c.url()
+        self.os_delimiter = c.os_delimiter
+        self.path_root = c.path_root
 
     def get_meta(self):
-        '''download source brach from github.
+        '''download source branch from github.
         store meta.json in self.source:dict
         '''
         self.source = {}
         try:
-            # with urllib.request.urlopen(self._url) as data:
             with urllib.request.urlopen(self.url["git_source"]) as data:
                 o = data.read()
-                # with open(self._path, "wb+") as pf:
-                with open(self.files["source.zip"], "wb+") as pf:
-                    pf.write(o)
-
-            with ZipFile(self.files["source.zip"]) as z:
-                if 'win_registry-source/meta.json' in z.namelist():
-                    self.source = json.loads(z.read('win_registry-source/meta.json'))
+            with open(self.files["source.zip"], "wb+") as pf:
+                pf.write(o)
+                pf.seek(0)
+                with ZipFile(pf) as z:
+                    if 'win_registry-source/meta.json' in z.namelist():
+                        self.source = json.loads(z.read('win_registry-source/meta.json'))
+            remove(f'{self.files["source.zip"]}')
         except Exception as e:
             # print(e)#pending: logging a least
             pass
 
-
     def new_version(self):
-        '''return True if new version available'''
+        '''return True if new version available by comparing
+        meta.json (source brach) vs local.json (files directory)
+        c'''
         if self.source.get("version") == None:
             return False
 
-        # path_local = fr"{path_root}/files/local.json"
         with open(self.file["local.json"]) as pf:
             local = json.load(pf)
 
         return self.source.get("version") > local.get("meta", {}).get("version", 0)
 
+    def upgrade(self):
+        pass
+
+
+
 if __name__ == "__main__":
     obj = Updater()
+    # obj.get_meta()
     obj.get_meta()
-    print(obj.source)
+    # print(obj.source)
     print(obj.new_version())
